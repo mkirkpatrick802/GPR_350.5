f256c5178478e0041bf8e5339df8fbc33f253aef
Michael F. Kirkpatrick <33790548+mkirkpatrick802@users.noreply.github.com>
2023-10-19 10:11:05 -0400

    Initial commit

---
+++ Assets/Scripts/Collision/CollisionManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using static CollisionDetection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CollisionManager : MonoBehaviour
+{
+    Octree tree;
+
+    public enum CollisionType
+    {
+        Standard,
+        Octree
+    }
+
+    public static CollisionType collisionType = CollisionType.Standard;
+
+    [SerializeField]
+    public uint nStartingParticles = 100;
+
+    [SerializeField]
+    private GameObject particlePrefab;
+
+    private void Start()
+    {
+        // TODO: YOUR CODE HERE
+        // Create the Octree. Create prefabs within the bounding box of the scene.
+    }
+
+    private void TreeCollisionResolution()
+    {
+        // TODO: YOUR CODE HERE
+        // Perform sphere-sphere collisions using the Octree
+    }
+
+    private void StandardCollisionResolution()
+    {
+        Sphere[] spheres = FindObjectsOfType<Sphere>();
+        PlaneCollider[] planes = FindObjectsOfType<PlaneCollider>();
+        for (int i = 0; i < spheres.Length; i++)
+        {
+            Sphere s1 = spheres[i];
+            for (int j = i + 1; j < spheres.Length; j++)
+            {
+                Sphere s2 = spheres[j];
+                ApplyCollisionResolution(s1, s2);
+            }
+            foreach (PlaneCollider plane in planes)
+            {
+                ApplyCollisionResolution(s1, plane);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CollisionChecks = 0;
+
+        // TODO: YOUR CODE HERE
+        // Call correct collision resolution type based
+        // on collisionType variable.
+    }
+
+    private void Update()
+    {
+        // TODO: YOUR CODE HERE
+        // Switch collision types if the "C" key is pressed.
+    }
+}
---
+++ Assets/Scripts/Collision/Octree/Octree.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public interface Octree
+{
+    /// <summary>
+    /// Inserts a particle into the octree, descending its children as needed.
+    /// </summary>
+    /// <param name="particle"></param>
+    public void Insert(Sphere particle);
+
+    /// <summary>
+    /// Does all necessary collision detection tests.
+    /// </summary>
+    public void ResolveCollisions();
+
+    /// <summary>
+    /// Removes all objects from the Octree.
+    /// </summary>
+    public void Clear();
+
+    /// <summary>
+    /// Creates a new Octree, properly creating children.
+    /// </summary>
+    /// <param name="pos">The position of this Octree</param>
+    /// <param name="halfWidth">The width of this Octree node, from the center to one edge (only needs to be used to calculate children's positions)</param>
+    /// <param name="depth">The number of levels beneath this one to create (i.e., depth = 1 means create one node with 8 children. depth = 0 means create only this node. depth = 2 means create one node with 8 children, each of which are Octree's with depth 1.</param>
+    /// <returns>The newly created Octree</returns>
+    public static Octree Create(Vector3 pos, float halfWidth = 1f, uint depth = 1)
+    {
+        // TODO: YOUR CODE HERE!
+        // Recursively call Create to initialize the Octree
+        return null;
+    }
+}
+
+/// <summary>
+/// An octree that holds 8 children, all of which are Octree's.
+/// </summary>
+public class OctreeNode : Octree
+{
+    public Vector3 position;
+    public Octree[] children;
+
+    // TODO: YOUR CODE HERE
+
+    /// <summary>
+    /// Inserts the given particle into the appropriate children. The particle
+    /// may need to be inserted into more than one child.
+    /// </summary>
+    /// <param name="sphere">The bounding sphere of the particle to insert.</param>
+    public void Insert(Sphere sphere)
+    {
+    }
+
+    /// <summary>
+    /// Resolves collisions in all children, as only leaf nodes can hold particles.
+    /// </summary>
+    public void ResolveCollisions()
+    {
+    }
+
+    /// <summary>
+    /// Removes all particles in each child.
+    /// </summary>
+    public void Clear()
+    {
+    }
+}
+
+/// <summary>
+/// An octree that holds only particles.
+/// </summary>
+public class OctreeObjects : Octree
+{
+    public ICollection<Sphere> Objects
+    {
+        get
+        {
+            // TODO: YOUR CODE HERE!
+            return null;
+        }
+    }
+
+    // TODO: YOUR CODE HERE!
+
+    /// <summary>
+    /// Inserts the particle into this node. It will be compared with all other
+    /// particles in this node in ResolveCollisions().
+    /// </summary>
+    /// <param name="particle">The particle to insert.</param>
+    public void Insert(Sphere particle)
+    {
+    }
+
+    /// <summary>
+    /// Calls CollisionDetection.ApplyCollisionResolution() on every pair of
+    /// spheres in this node.
+    /// </summary>
+    public void ResolveCollisions()
+    {
+    }
+
+    /// <summary>
+    /// Removes all objects from this node.
+    /// </summary>
+    public void Clear()
+    {
+    }
+}
